0cd6cc2984857e95fdce2281f18df4318813c1e9
rgl14 <52996923+rgl14@users.noreply.github.com>
2020-02-26 20:40:37 +0530

    added bm betslips in module

---
+++ src/app/bmbetslips/bmbetslips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bmbetslips',
+  templateUrl: './bmbetslips.component.html',
+  styleUrls: ['./bmbetslips.component.css']
+})
+export class BmbetslipsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
